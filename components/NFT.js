0d3a60736fae3a6356866749211941933d8c157b
unknown <nishant.guvvada@gmail.com>
2025-01-10 19:45:45 +0530

    upload fixed

--- components/NFT.js
+++ components/NFT.js
@@ -10,6 +10,9 @@ export const NFT = () => {
 
     const [collectionImage, setCollectionImage] = useState(null);
     const [nftImage, setNftImage] = useState(null);
+    const [collectionName, setCollectionName] = useState("My Default Collection");
+    const [collectionSymbol, setCollectionSymbol] = useState("MDC");
+    const [collectionDescription, setCollectionDescription] = useState("Description of the collection");
 
     const nftRef = useRef();
     const collectionRef = useRef();
@@ -17,7 +20,7 @@ export const NFT = () => {
     const handleCreateCollection = () => {
         try {
             if (wallet.publicKey != null) {
-                createCollection(collectionImage, wallet, connection);
+                createCollection(collectionImage, wallet, connection, collectionName, collectionSymbol, collectionDescription);
             }
         } catch(err) {
             console.log("Error handling collection!");
@@ -26,30 +29,50 @@ export const NFT = () => {
 
     return (
         <>
-        <div id="collection" className="flex flex-col gap-2 place-items-center p-6 bg-white border border-gray-200 rounded-lg shadow">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Create a collection</h5>
-            <label className="block mb-2 text-sm font-medium text-gray-900">Upload file</label>
-            <div className="mb-2 flex flex-row gap-2">
-                <input accept="image/*" ref={collectionRef} onChange={(e) => {
-                    setCollectionImage(e.target.files[0])
-                }} className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50" type="file"/>
-                <button className="inline" onClick={()=>{collectionRef.current.value = ""; setCollectionImage(null)}}><ImCross /></button>
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">NFT Marketplace</h1>
+        <blockquote className="text-xl italic font-semibold text-gray-900 dark:text-white">
+            <p>"NFT Marketplace is a sample SOLANA project intended to allow users to create NFT collection and add child NFTs to the collection."</p>
+        </blockquote>
+        <span className="underline underline-offset-3 decoration-4 decoration-white text-white bg-blue-700 rounded p-2">{wallet.publicKey == null ? "No wallet connected" : <div>{wallet.publicKey.toBase58()}</div>}</span>
+        <div className="w-screen flex flex-row justify-center items-center gap-20">
+            <div id="collection" className="flex flex-col gap-2 place-items-center p-6 bg-white border border-gray-200 rounded-lg shadow">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Create a collection</h5>
+                <div className="w-full">
+                    <label className="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-white">Collection Name</label>
+                    <input onChange={(e) => { setCollectionName(e.target.value)}} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Collection Name" required />
+                    <label className="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-white">Symbol</label>
+                    <input onChange={(e) => { setCollectionSymbol(e.target.value)}} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Symbol" required />
+                    <label className="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                    <input onChange={(e) => { setCollectionDescription(e.target.value)}} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Description" required />
+                </div>
+                <label className="block mt-4 mb-2 text-sm font-medium text-gray-900">Upload file</label>
+                <div className="mb-2 flex flex-row gap-2">
+                    <input accept="image/*" ref={collectionRef} onChange={(e) => {
+                        setCollectionImage(e.target.files[0])
+                    }} className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50" type="file"/>
+                    <button className="inline" onClick={()=>{collectionRef.current.value = ""; setCollectionImage(null)}}><ImCross /></button>
+                </div>
+                {collectionImage == null ? "" : <img src={URL.createObjectURL(collectionImage)} className="object-scale-down w-24 h-24"/>}
+                <button onClick={handleCreateCollection} type="button" className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-md text-sm px-5 py-2.5 text-center mb-2">Create Collection</button>
             </div>
-            {collectionImage == null ? "" : <img src={URL.createObjectURL(collectionImage)} className="object-scale-down w-24 h-24"/>}
-            <button onClick={handleCreateCollection} type="button" className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-md text-sm px-5 py-2.5 text-center mb-2">Create Collection</button>
-        </div>
-        <div id="nft" className="flex flex-col gap-2 place-items-center p-6 bg-white border border-gray-200 rounded-lg shadow">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Create & add NFT to collection</h5>
-            <label className="block mb-2 text-sm font-medium text-gray-900">Upload file</label>
-            <div className="mb-2 flex flex-row gap-2">
-                <input accept="image/*" ref={nftRef} onChange={(e) => {
-                    setNftImage(e.target.files[0])
-                }} className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50" type="file"/>
-                <button onClick={()=>{nftRef.current.value = ""; setNftImage(null)}}><ImCross /></button>
+            <div id="nft" className="flex flex-col gap-2 place-items-center p-6 bg-white border border-gray-200 rounded-lg shadow">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Create & add NFT to collection</h5>
+                <div className="w-full">
+                    <label className="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-white">NFT Name</label>
+                    <input onChange={(e) => { setCollectionName(e.target.value)}} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="NFT Name" required />
+                    <label className="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-white">Symbol</label>
+                    <input onChange={(e) => { setCollectionSymbol(e.target.value)}} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="NFT Symbol" required />
+                </div>
+                <label className="block mt-4 mb-2 text-sm font-medium text-gray-900">Upload file</label>
+                <div className="mb-2 flex flex-row gap-2">
+                    <input accept="image/*" ref={nftRef} onChange={(e) => {
+                        setNftImage(e.target.files[0])
+                    }} className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50" type="file"/>
+                    <button onClick={()=>{nftRef.current.value = ""; setNftImage(null)}}><ImCross /></button>
+                </div>
+                {nftImage == null ? "" : <img src={URL.createObjectURL(nftImage)} className="object-scale-down w-24 h-24"/>}
+                <button type="button" className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-md text-sm px-5 py-2.5 text-center mb-2">Create & Add NFT</button>
             </div>
-            {wallet.publicKey == null ? "" : <div>{wallet.publicKey.toBase58()}</div>}
-            {nftImage == null ? "" : <img src={URL.createObjectURL(nftImage)} className="object-scale-down w-24 h-24"/>}
-            <button type="button" className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-md text-sm px-5 py-2.5 text-center mb-2">Create & Add NFT</button>
         </div>
         </>
     )
--- utils/create-collection.js
+++ utils/create-collection.js
@@ -5,21 +5,20 @@ import {
 import {
     generateSigner,
     percentAmount,
-    createGenericFileFromBrowserFile,
-    createNoopSigner,
     signerIdentity,
 } from "@metaplex-foundation/umi";
-import { fromWeb3JsPublicKey, toWeb3JsPublicKey, toWeb3JsTransaction, toWeb3JsKeypair, toWeb3JsLegacyTransaction } from "@metaplex-foundation/umi-web3js-adapters";
+import { fromWeb3JsPublicKey, toWeb3JsKeypair, toWeb3JsLegacyTransaction } from "@metaplex-foundation/umi-web3js-adapters";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
-import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
-import { Connection, PublicKey } from "@solana/web3.js";
+import { WebUploader } from "@irys/web-upload";
+import { WebSolana } from "@irys/web-upload-solana";
 
-export const createCollection = async (inputImage, user, connection) => {
+export const createCollection = async (inputImage, user, connection, collectionName, collectionSymbol, collectionDescription) => {
 
-    const walletBalance = await connection.getBalance(user.publicKey);
+    // Checking wallet balance for debugging
+    // const walletBalance = await connection.getBalance(user.publicKey);
 
-    console.log("Wallet Balance: ", walletBalance);
-    console.log("Wallet: ", user.publicKey.toBase58());
+    // console.log("Wallet Balance: ", walletBalance);
+    // console.log("Wallet: ", user.publicKey.toBase58());
 
     const umi = createUmi(connection);
 
@@ -30,43 +29,68 @@ export const createCollection = async (inputImage, user, connection) => {
         signAllTransactions: async (txs) => txs,
     }
 
-    // generate mint keypair
+    // Generate mint keypair
     const collectionMint = generateSigner(umi);
     const web3jsCollectionMint = toWeb3JsKeypair(collectionMint);
 
     umi
         .use(signerIdentity(signer))
         .use(mplTokenMetadata())
-        .use(irysUploader());
+
+    // Create an instance of irys uploader
+    const irysUploader = await WebUploader(WebSolana).withProvider(user).withRpc("https://api.devnet.solana.com").devnet();
+
+	// Get cost to upload "inputImage.size" bytes
+	const imagePrice = await irysUploader.getPrice(inputImage.size);
+	console.log(`Uploading ${inputImage.size} bytes costs ${irysUploader.utils.fromAtomic(imagePrice)}`);
+	await irysUploader.fund(imagePrice);
+
+    // Upload the image
+    const image = await irysUploader.uploadFile(inputImage);
+    console.log(`Image uploaded ==> https://gateway.irys.xyz/${image.id}`);
     
-    const file = await createGenericFileFromBrowserFile(inputImage);
-    const [image] = await umi.uploader.upload([file]);
-    console.log("image uri:", image);
-    
-    // upload offchain json to Arweave using irys
-    const uri = await umi.uploader.uploadJson({
-        name: "NX 2 Collection",
-        symbol: "NX 2",
-        description: "NX 2 Collection is a sample created to add to the portfolio projects.",
-        image,
+    // Create metadata
+    const metadata = {
+        name: collectionName,
+        symbol: collectionSymbol,
+        description: collectionDescription,
+        image: `https://gateway.irys.xyz/${image.id}`,
+    }
+
+    const tags = [{ name: "Content-Type", value: "application/json" }];
+
+    // Obtain size of the metadata
+    const jsonSize = Object.keys(metadata).length;
+    // Get cost to upload "jsonSize" bytes
+    const jsonPrice = await irysUploader.getPrice(jsonSize);
+
+    console.log(`Uploading ${jsonSize} bytes costs ${irysUploader.utils.fromAtomic(jsonPrice)}`);
+	await irysUploader.fund(jsonPrice);
+
+    // Convert the JS object to a JSON file
+    const blob = new Blob([JSON.stringify(metadata)], {
+        type: 'application/json',
     });
-    console.log("Collection offchain metadata URI:", uri);
+
+    // Upload the JSON file
+    const json = await irysUploader.uploadFile(blob, { tags: tags });
+    console.log(`Collection offchain metadata uploaded ==> https://gateway.irys.xyz/${json.id}`);
     
     // create and mint NFT
     const tx = await createNft(umi, {
         mint: collectionMint,
-        name: "NX 2 Collection",
-        symbol: "NX 2",
-        uri: "uri sample",
+        name: collectionName,
+        symbol: collectionSymbol,
+        uri: `https://gateway.irys.xyz/${json.id}`,
         updateAuthority: user.publicKey,
         sellerFeeBasisPoints: percentAmount(0),
         isCollection: true,
     }).buildWithLatestBlockhash(umi);
 
-    // const transaction = await collectionMint.signTransaction(tx);
-
+    // convert umi transaction to web3js
     const web3jsTransaction = toWeb3JsLegacyTransaction(tx);
     
+    // add wallet as the fee payer
     web3jsTransaction.feePayer = user.publicKey;
 
     // get latest blockhash
@@ -90,9 +114,14 @@ export const createCollection = async (inputImage, user, connection) => {
     console.log("✅ Finished successfully!");
 }
 
-// Wallet Balance:  8405173160
-// image uri: https://arweave.net/8pqsd945LGN2z5T8vwFn7mFPGS9oaNxh3rPoELph4uC5
-// Collection offchain metadata URI: https://arweave.net/32LbgGpj84kuSkAefZdFxhsXPCsHgNBw7hL8BNeoC5Mm
-// Collection NFT:  https://explorer.solana.com/address/AsSkj8Jtar3pn35xYCECxXLw7ngN25HT5iozj2ADHzvU?cluster=devnet
-// Collection NFT address is: AsSkj8Jtar3pn35xYCECxXLw7ngN25HT5iozj2ADHzvU
+// Wallet Balance:  9306050408
+// Wallet:  4H6oyyKmzYuV6PeeTGDayQdH8BSyvdpXYNcgyLSoXs8P
+// Uploading 71144 bytes costs 0.00000921
+// Image uploaded ==> https://gateway.irys.xyz/CzdEa7MbcFzfJhn5eyfhUerPjRFWvixyKMoq2Yy8esDb
+// Uploading 4 bytes costs 0.000009221
+// Collection offchain metadata URI: ==> https://gateway.irys.xyz/CUusHJBJpFvTJpjs9j83V4yVtuU2Q1AbU1CvXEXDsigT
+// Signed Transaction Transaction {signatures: Array(2), feePayer: PublicKey, instructions: Array(2), recentBlockhash: '24uyMoCRBDXoHqW8J1orMn5GmQ2D9NYPFADWdfK2T42L', lastValidBlockHeight: undefined, …}
+// Transaction ID 549DvMQDyWJsHaxXwygdZCXKyznwhLRvUdWVk1C1t8NUnbYcBFSkJJqU4fGAqmAvAxynx4yMZBcBGysjfqM85z35
+// sign {context: {…}, value: {…}}
+// Collection NFT address is: FAXRx8BNMyykt1PE8ixmM8Ca5VPNx4iFxJL5uczVnxXx
 // ✅ Finished successfully!
